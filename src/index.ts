d82671ee2ba83b348675cc436103ad8856d17d2c
danovantaa <zefanyadanovantaa@gmail.com>
2024-06-27 17:17:01 +0700

    init : initialize tailwind

--- src/index.ts
+++ src/index.ts
@@ -1,97 +1,102 @@
 interface Task {
-    text : string;
-    completed : boolean;
+  text: string;
+  completed: boolean;
 }
 
 const taskInput = document.getElementById("taskInput") as HTMLInputElement;
-const addTaskButton = document.getElementById ("addTaskBtn") as HTMLButtonElement;
-const taskList = document.getElementById ("taskList") as HTMLUListElement;
-const filterSelect = document.getElementById ("filterSelect") as HTMLSelectElement;
+const addTaskBtn = document.getElementById("addTaskBtn") as HTMLButtonElement;
+const taskList = document.getElementById("taskList") as HTMLUListElement;
+const filterSelect = document.getElementById(
+  "filterSelect"
+) as HTMLSelectElement;
 
-let tasks : Task[] = JSON.parse(localStorage.getItem("tasks") || null ) || [] ;
+let tasks: Task[] = JSON.parse(localStorage.getItem("tasks") || null) || [];
 
-const renderTasks = () : void =>{
-    taskList.innerHTML= "";
-    const filter = filterSelect.value;
+const renderTasks = () => {
+  taskList.innerHTML = "";
 
-    const filteredTasks = filter === "all" 
-    ? tasks 
-    : filter === "complete" 
-    ? tasks.filter (task =>task.completed) 
-    : tasks.filter(task => !task.completed);
-    
-     filteredTasks.forEach((task, index) => {
-        const li = document.createElement("li");
-        li.classList.add("task");
-        li.textContent = task.text;
-        if(task.completed) li.classList.add("completed");
-    
-        const statusContainer = document.createElement("div");
-        statusContainer.classList.add("status-container");
-    
-        const completedBtn = document.createElement("button");
-        completedBtn.textContent = task.completed ? "Ulangi" : "Selesai";
-        completedBtn.addEventListener ("click", () => { toggleCompleted(index) });
-    
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "Hapus ";
-        deleteBtn.addEventListener ("click", () => { deleteTask(index) });
-    
-        const editBtn = document.createElement("button");
-        editBtn.textContent = "Edit";
-        editBtn.addEventListener ("click", () => { editTask(index) });
-    
-        statusContainer.appendChild(completedBtn); 
-        statusContainer.appendChild(deleteBtn); 
-        statusContainer.appendChild(editBtn); 
-    
-        li.appendChild(statusContainer);
-        taskList.appendChild(li);
-    });
-}
+  const filter = filterSelect.value;
 
-function toggleCompleted (index : number) { 
-    tasks[index].completed = !tasks[index].completed;
+  const filteredTasks =
+    filter === "all"
+      ? tasks
+      : filter === "complete"
+      ? tasks.filter((task) => task.completed)
+      : tasks.filter((task) => !task.completed);
+
+  filteredTasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.classList.add("task");
+    li.textContent = task.text;
+    if (task.completed) li.classList.add("completed");
+    li.classList.add("w-full", "rounded", "flex", "items-center", "mb-2", "px-4", "py-2", "border", "border-gray-300", "bg-gray-100", "mx-auto", "text-lg", "justify-between");
+
+    const statusContainer = document.createElement("div");
+    statusContainer.classList.add("status-container", "flex", "items-center");
+
+    const completedBtn = document.createElement("button");
+    completedBtn.textContent = task.completed ? "Ulangi" : "Selesai";
+    completedBtn.addEventListener("click", () => toggleCompleted(index));
+    completedBtn.classList.add("bg-blue", "text-white", "font-bold", "py-2", "px-4", "rounded", "focus:outline-none", "focus:shadow-outline");
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Hapus";
+    deleteBtn.addEventListener("click", () => deleteTask(index));
+    deleteBtn.classList.add("bg-red", "text-white", "font-bold", "py-2", "px-4", "rounded", "focus:outline-none", "focus:shadow-outline");
+
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.addEventListener("click", () => editTask(index));
+    editBtn.classList.add("bg-green", "text-white", "font-bold", "py-2", "px-4", "rounded", "focus:outline-none", "focus:shadow-outline");
+
+    statusContainer.appendChild(completedBtn);
+    statusContainer.appendChild(deleteBtn);
+    statusContainer.appendChild(editBtn);
+
+    li.appendChild(statusContainer);
+    taskList.appendChild(li);
+  });
+};
+
+const toggleCompleted = (index: number): void => {
+  tasks[index].completed = !tasks[index].completed;
+  saveTasks();
+  renderTasks();
+};
+
+const addTask = (): void => {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    tasks.push({ text: taskText, completed: false });
+    taskInput.value = "";
     saveTasks();
     renderTasks();
-}
+  }
+};
 
-function addTask (){ 
-    const taskText = taskInput.value.trim();
-    if(taskText !== ""){
-        tasks.push ({ text: taskText, completed : false});
-        taskInput.value = "";
-        saveTasks();
-        renderTasks();
-    };
-}
+const deleteTask = (index: number): void => {
+  tasks.splice(index, 1);
+  saveTasks();
+  renderTasks();
+};
 
-function deleteTask (index : number) { 
-    tasks.splice(index,1);
+const editTask = (index: number): void => {
+  const newText = prompt("Edit task", tasks[index].text);
+  if (newText !== null && newText.trim() !== "") {
+    tasks[index].text = newText;
     saveTasks();
     renderTasks();
-}
+  }
+};
 
-function editTask (index : number){ 
-    const newText = prompt("Edit Task", tasks[index].text);
-    if(newText !== null && newText.trim() != ""){
-        tasks[index].text = newText;
-        saveTasks();
-        renderTasks();
-    }
-}
+const saveTasks = (): void => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
-const saveTasks = () : void => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-
-addTaskButton.addEventListener("click", addTask);
-taskInput.addEventListener("keypress",(event) => {
-    if(event.key === "Enter") addTask();
+addTaskBtn.addEventListener("click", addTask);
+taskInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") addTask();
 });
 filterSelect.addEventListener("change", renderTasks);
 
 renderTasks();
-
-
